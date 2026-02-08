43f815547bca27cf1abc2d441c44c15d4be081d7
Harshit <harshkush15sep@gmail.com>
2026-02-08 15:54:17 +0530

    further task ahead completion

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+
+import { CartProvider } from './context/CartContext';
+
+export default function App() {
+  return (
+    <CartProvider>
+      <h1>Naksh Jewels</h1>
+    </CartProvider>
+  );
+}
